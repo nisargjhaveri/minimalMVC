9572c4d147680333ec1525d4d90954cdfa150152
Nisarg Jhaveri <nisargjhaveri@gmail.com>
2015-04-14 01:40:29 +0530

    load_fragment in views

--- sys/view.php
+++ sys/view.php
@@ -2,9 +2,17 @@
 
 class View {
 
+    private $data = null;
+
     function __construct($view_name_noconflict, $data = array()) {
+        $this->data = $data;
         extract($data);
         include(APPPATH . "views/$view_name_noconflict.php");
+    }
+
+    function load_fragment($frag_name_noconflict) {
+        extract($this->data);
+        include(APPPATH . "views/$frag_name_noconflict.php");
     }
 
 }
