33cd2f2a4d5d6527c1c580470e011ad3e3ac9e20
Nisarg Jhaveri <nisargjhaveri@gmail.com>
2015-10-19 16:54:01 +0530

    Allow sending data to fragments from views
    - Also fix possible name conflict in views

--- view.php
+++ view.php
@@ -4,14 +4,24 @@ class View {
 
     public $data = null;
 
-    function __construct($view_name_noconflict, $data = array()) {
-        $this->data = $data;
-        extract($data);
+    function __construct($view_name_noconflict, $data_noconflict = array()) {
+        $this->data = $data_noconflict;
+        extract($this->data);
         include(APPPATH . "views/$view_name_noconflict.php");
     }
 
-    function load_fragment($frag_name_noconflict) {
+    /**
+     * Load a view fragment
+     *
+     * Data passed to main view is already passed, you can optionally extend or
+     * override some data
+     *
+     * @param string $frag_name_noconflict Name of the view fragment
+     * @param array $data_noconflict (optional) Data as an associative array
+     */
+    function load_fragment($frag_name_noconflict, $data_noconflict = array()) {
         extract($this->data);
+        extract($data_noconflict);
         include(APPPATH . "views/$frag_name_noconflict.php");
     }
 
